1ff0a130a4236bc87d986fe33f4ffd0b853774fe
Frank Albarracín <molina.frank@gmail.com>
2024-09-17 23:58:43 -0400

    one review

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+   site: "https://happytailgrooming.com/",
    output: 'hybrid',
    adapter: netlify({
       cacheOnDemandPages: true,
